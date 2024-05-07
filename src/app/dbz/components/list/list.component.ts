6cb83f6d882220b5ce0c753ba9bfaf1d39d98d64
Miguel Morales <miguelmsaguilar@gmail.com>
2024-05-07 11:49:23 -0500

    se creo interface characters

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ]
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,5 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MainPageComponent {
+  public characters: Character[] = [
+    {
+      name: 'krilin',
+      power: 1000
+    },
+    {
+      name: 'goku',
+      power: 10000
+    },
+    {
+      name: 'vegeta',
+      power: 7500
+    },
 
+  ];
 }
